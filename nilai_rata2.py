e47bc716df1676a8796f9878593c5f5494d4be22
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-24 16:27:14 +0700

    add file nilai_rata2.py soal algoritma tentang konversi suhu

---
+++ nilai_rata2.py
@@ -0,0 +1,15 @@
+"""
+Soal : terdapat dua bilangan x dan y. berapa nilai rata-ratanya?
+Penyelesaian :
+masukan : x dan y
+keluaran : rerata
+rumus : rerata <- (x+y)/2
+pseudokode :
+masukkan x dan y
+rerata <- (x+y)/2
+tampilkan rerata
+"""
+x = 4
+y = 5
+hasil = (x+y)/2
+print(hasil)
