be6e737225af76c0c51204cbb4a2ada4905c85aa
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-02-24 16:05:11 +0700

    soal algoritma tentang konversi suhu

---
+++ konversi_suhu.py
@@ -0,0 +1,16 @@
+"""
+soal : suatu sensor menghasilkan suhu dalam derajat celsius. bagaimana cara menampilkan suhu dalam derajat fahrenheit?
+
+Penyelesaian :
+Masukan : Celsius
+Keluaran : Fahrenit
+rumus konversi celcius ke fahrenheit : 9/5 * celcius + 32
+pseudokode :
+masukan celcius
+fahreint <- 1,8 * celcius + 32
+tampilkan fahrenheit
+"""
+celcius = 30
+fahrenheit = 1.8 * celcius + 32
+hasil = 1.8 * celcius + 32
+print(hasil)
